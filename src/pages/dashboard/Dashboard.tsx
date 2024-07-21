6c98d195a8414a798457eed77eee57e9bf153078
Anthony Hall <apexa-antkky@outlook.com>
2024-07-21 04:30:16 -0700

    dashboard layout

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,3 +1,22 @@
 export default function DashboardPage() {
-    return <></>;
+    return (
+        <div className="h-full flex flex-wrap gap-5 overflow-y-scroll">
+            {/* Left Section */}
+            <section className="h-full w-4/6 flex flex-col gap-5">
+                {/* Equity */}
+                <div className="bg-purple w-full h-1/2 rounded-3xl"></div>
+
+                {/* Left Bottom */}
+                <section className="flex gap-5 grow">
+                    <div className="bg-purple w-1/2 rounded-3xl"></div>
+                    <div className="bg-purple w-1/2 rounded-3xl"></div>
+                </section>
+            </section>
+
+            {/* Right Section */}
+            <section className="grow flex flex-col gap-5">
+                <div className="bg-purple rounded-3xl w-full h-full"></div>
+            </section>
+        </div>
+    );
 }
