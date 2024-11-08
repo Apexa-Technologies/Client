4c6732a021f29b68b689ae65c842048f132e9a03
Anthony Hall <apexa-antkky@outlook.com>
2024-11-08 01:31:50 -0800

    journal page switching

---
+++ .VSCodeCounter/2024-11-06_02-20-03/results.md
@@ -0,0 +1,74 @@
+# Summary
+
+Date : 2024-11-06 02:20:03
+
+Directory c:\\Users\\Antik\\OneDrive\\Documents\\GitHub\\Client
+
+Total : 71 files,  2364 codes, 61 comments, 215 blanks, all 2640 lines
+
+Summary / [Details](details.md) / [Diff Summary](diff.md) / [Diff Details](diff-details.md)
+
+## Languages
+| language | files | code | comment | blank | total |
+| :--- | ---: | ---: | ---: | ---: | ---: |
+| TypeScript JSX | 38 | 1,936 | 7 | 160 | 2,103 |
+| TypeScript | 3 | 151 | 0 | 16 | 167 |
+| JSON | 2 | 83 | 0 | 2 | 85 |
+| SCSS | 6 | 69 | 0 | 10 | 79 |
+| XML | 16 | 49 | 0 | 16 | 65 |
+| JavaScript | 1 | 41 | 52 | 1 | 94 |
+| HTML | 2 | 25 | 0 | 5 | 30 |
+| Rust | 2 | 9 | 2 | 4 | 15 |
+| CSS | 1 | 1 | 0 | 1 | 2 |
+
+## Directories
+| path | files | code | comment | blank | total |
+| :--- | ---: | ---: | ---: | ---: | ---: |
+| . | 71 | 2,364 | 61 | 215 | 2,640 |
+| . (Files) | 2 | 50 | 0 | 3 | 53 |
+| dist | 5 | 61 | 52 | 7 | 120 |
+| dist (Files) | 1 | 13 | 0 | 3 | 16 |
+| dist\\assets | 4 | 48 | 52 | 4 | 104 |
+| src | 61 | 2,199 | 7 | 200 | 2,406 |
+| src (Files) | 4 | 102 | 0 | 16 | 118 |
+| src-tauri | 3 | 54 | 2 | 5 | 61 |
+| src-tauri (Files) | 2 | 48 | 0 | 2 | 50 |
+| src-tauri\\src | 1 | 6 | 2 | 3 | 11 |
+| src\\api | 1 | 109 | 0 | 7 | 116 |
+| src\\assets | 14 | 43 | 0 | 14 | 57 |
+| src\\components | 10 | 202 | 0 | 28 | 230 |
+| src\\components\\inputs | 8 | 119 | 0 | 19 | 138 |
+| src\\components\\sidebar | 1 | 61 | 0 | 5 | 66 |
+| src\\components\\small | 1 | 22 | 0 | 4 | 26 |
+| src\\constants | 1 | 14 | 0 | 4 | 18 |
+| src\\layouts | 1 | 125 | 0 | 15 | 140 |
+| src\\pages | 28 | 1,516 | 7 | 109 | 1,632 |
+| src\\pages\\calculator | 2 | 14 | 0 | 2 | 16 |
+| src\\pages\\calender | 1 | 14 | 0 | 1 | 15 |
+| src\\pages\\dashboard | 13 | 1,009 | 7 | 69 | 1,085 |
+| src\\pages\\dashboard (Files) | 1 | 42 | 6 | 4 | 52 |
+| src\\pages\\dashboard\\charts | 1 | 120 | 1 | 3 | 124 |
+| src\\pages\\dashboard\\components | 11 | 847 | 0 | 62 | 909 |
+| src\\pages\\dashboard\\components\\modals | 4 | 309 | 0 | 15 | 324 |
+| src\\pages\\dashboard\\components\\topbar | 1 | 111 | 0 | 12 | 123 |
+| src\\pages\\dashboard\\components\\widgets | 6 | 427 | 0 | 35 | 462 |
+| src\\pages\\dashboard\\components\\widgets\\calculator | 1 | 51 | 0 | 3 | 54 |
+| src\\pages\\dashboard\\components\\widgets\\current | 1 | 22 | 0 | 2 | 24 |
+| src\\pages\\dashboard\\components\\widgets\\days | 1 | 117 | 0 | 9 | 126 |
+| src\\pages\\dashboard\\components\\widgets\\equity | 1 | 22 | 0 | 2 | 24 |
+| src\\pages\\dashboard\\components\\widgets\\notes | 1 | 93 | 0 | 8 | 101 |
+| src\\pages\\dashboard\\components\\widgets\\trades | 1 | 122 | 0 | 11 | 133 |
+| src\\pages\\journal | 2 | 171 | 0 | 7 | 178 |
+| src\\pages\\journal (Files) | 1 | 41 | 0 | 2 | 43 |
+| src\\pages\\journal\\components | 1 | 130 | 0 | 5 | 135 |
+| src\\pages\\journal\\components\\topbar | 1 | 130 | 0 | 5 | 135 |
+| src\\pages\\login | 2 | 77 | 0 | 7 | 84 |
+| src\\pages\\news | 2 | 14 | 0 | 2 | 16 |
+| src\\pages\\settings | 4 | 87 | 0 | 7 | 94 |
+| src\\pages\\settings (Files) | 1 | 66 | 0 | 4 | 70 |
+| src\\pages\\settings\\subpages | 3 | 21 | 0 | 3 | 24 |
+| src\\pages\\setup | 2 | 130 | 0 | 14 | 144 |
+| src\\routes | 1 | 60 | 0 | 2 | 62 |
+| src\\types | 1 | 28 | 0 | 5 | 33 |
+
+Summary / [Details](details.md) / [Diff Summary](diff.md) / [Diff Details](diff-details.md)
--- src/pages/journal/journal.tsx
+++ src/pages/journal/journal.tsx
@@ -1,14 +1,203 @@
-import { useState } from "react";
-import Button2 from "../../components/inputs/button2";
+import { useEffect, useState } from "react";
 import Input2 from "../../components/inputs/input2";
-import JournalPageTopBar from "./components/topbar/journal";
+import { useNavigate } from "react-router-dom";
+import { RoutePath } from "../../constants/path";
+import Button2 from "../../components/inputs/button2";
+import Search1 from "../../components/inputs/search1";
+import settings from "../../assets/settings.svg";
+import help from "../../assets/help.svg";
 
 export default function JournalPage() {
-    const [title, setTitle] = useState("Untitled Document");
-    const date = "Aug, 6th";
+    const data = [
+        {
+            day: 15,
+            month: 11,
+            data: {
+                title: "Untitled Document",
+            },
+        },
+        {
+            day: 16,
+            month: 11,
+            data: {
+                title: "Untitled Document",
+            },
+        },
+        {
+            day: 17,
+            month: 11,
+            data: {
+                title: "Untitled Document",
+            },
+        },
+        {
+            day: 18,
+            month: 11,
+            data: {
+                title: "Untitled Document",
+            },
+        },
+        {
+            day: 19,
+            month: 11,
+            data: {
+                title: "Untitled Document",
+            },
+        },
+        {
+            day: 20,
+            month: 11,
+            data: {
+                title: "Untitled Document",
+            },
+        },
+        {
+            day: 21,
+            month: 11,
+            data: {
+                title: "Untitled Document",
+            },
+        },
+        {
+            day: 22,
+            month: 11,
+            data: {
+                title: "Untitled Document",
+            },
+        },
+        {
+            day: 23,
+            month: 11,
+            data: {
+                title: "Untitled Document",
+            },
+        },
+        {
+            day: 24,
+            month: 11,
+            data: {
+                title: "Monday NY Bull Open",
+                journal: "test test test test",
+                trades: [
+                    {
+                        entry: 123,
+                        stop: 123,
+                        take: 123,
+                        riskReward: 0,
+                        profit: 250,
+                        equity: 10250,
+                        time: "Wed Nov 06 2024 02:18:07 GMT-0800 (Pacific Standard Time)",
+                        pair: "XAUUSD",
+                        notes: "",
+                        _id: "672b425fdb30f17c1a703bef",
+                    },
+                    {
+                        entry: 123,
+                        stop: 123,
+                        take: 123,
+                        riskReward: 0,
+                        profit: 500,
+                        equity: 10750,
+                        time: "Wed Nov 06 2024 02:18:12 GMT-0800 (Pacific Standard Time)",
+                        pair: "XAUUSD",
+                        notes: "",
+                        _id: "672b4264db30f17c1a703c0a",
+                    },
+
+                    {
+                        entry: 324,
+                        stop: 234,
+                        take: 234,
+                        riskReward: 0,
+                        profit: 234,
+                        equity: 10984,
+                        time: "Wed Nov 06 2024 23:00:20 GMT-0800 (Pacific Standard Time)",
+                        pair: "XAUUSD",
+                        notes: "",
+                        _id: "672c6584483d14946fe43e5b",
+                    },
+                ],
+            },
+        },
+        {
+            day: 25,
+            month: 11,
+            data: {
+                title: "Untitled Document",
+            },
+        },
+    ];
+
+    const navigate = useNavigate();
+
+    const currentTime = new Date();
+    const todaysMonth = currentTime.getMonth() + 1;
+    const today = currentTime.getDate();
+
+    const [title, setTitle] = useState("");
+
+    const [currentday, setCurrentDay] = useState({
+        title: "Untitled Document",
+        date: "",
+    });
+
+    useEffect(() => {
+        setTitle(currentday.title);
+    }, [currentday]);
+
+    function Days() {
+        return data.slice(-10).map((day: any) => {
+            return (
+                <div
+                    onClick={() => {
+                        setCurrentDay({
+                            title: day.data.title || "Untitled Document",
+                            date: "test",
+                        });
+                    }}
+                >
+                    <h1 className="text-2xl font-medium hover:opacity-100 opacity-75 cursor-pointer">
+                        {day.day}
+                    </h1>
+                </div>
+            );
+        });
+    }
+
+    function Topbar_Buttons() {
+        return (
+            <div className="flex gap-3">
+                <button
+                    className="p-2.5 rounded-full hover:opacity-70 transition-all bg-darkprimary/80"
+                    onClick={() =>
+                        navigate(RoutePath.Settings, { replace: true })
+                    }
+                >
+                    <img src={settings} />
+                </button>
+                <button className="p-2.5 rounded-full hover:opacity-70 transition-all bg-darkprimary/80">
+                    <img src={help} />
+                </button>
+            </div>
+        );
+    }
+
     return (
         <div className="flex pb-10 gap-4 h-screen flex-col overflow-y-scroll">
-            <JournalPageTopBar />
+            <div className="relative rounded-full bg-primary flex items-center p-0 pl-5 pr-5 justify-between">
+                <div className="">
+                    <Search1 />
+                </div>
+                <div className="h-16 pt-2 pb-2 flex gap-16 items-center">
+                    <Days />
+                    <div>
+                        <Button2 text="Today" />
+                    </div>
+                </div>
+                <div>
+                    <Topbar_Buttons />
+                </div>
+            </div>
             <div className="min-h-full h-fit flex flex-wrap gap-5 overflow-y-hidden">
                 <section className="w-4/6 flex flex-col gap-5">
                     <div className="h-fit bg-primary rounded-[2rem] flex flex-col p-5 pl-8 pr-8">
@@ -21,7 +210,7 @@ export default function JournalPage() {
                                     ) => setTitle(e.target.value)}
                                 />
                             </div>
-                            <h1 className="text-4xl">{date}</h1>
+                            <h1 className="text-4xl">{currentday.date}</h1>
                         </div>
                         <hr></hr>
                         <div className="w-full h-1/2 flex justify-end items-center">
